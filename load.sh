4833feee4845681c6327aa7277798bf09f1b5746
forrestchen <forrestchen@tencent.com>
2019-10-12 18:26:17 +0800

    fix build bug
    Signed-off-by: forrestchen <forrestchen@tencent.com>

--- load.sh
+++ load.sh
@@ -3,10 +3,10 @@
 make -C bpf/sockops clean
 make -C bpf/sockops
 
-bpftool -m prog load bpf_sockops.o "/sys/fs/bpf/bpf_sockop"
+bpftool -m prog load bpf/sockops/bpf_sockops.o "/sys/fs/bpf/bpf_sockop"
 bpftool cgroup attach "/sys/fs/cgroup/unified/" sock_ops pinned "/sys/fs/bpf/bpf_sockop"
 MAP_ID=$(sudo bpftool prog show pinned "/sys/fs/bpf/bpf_sockop" | grep -o -E 'map_ids [0-9]+' | awk '{print $2}')
 bpftool map pin id $MAP_ID "/sys/fs/bpf/sock_ops_map"
-bpftool -m prog load bpf_redir.o "/sys/fs/bpf/bpf_redir" map name sock_ops_map pinned "/sys/fs/bpf/sock_ops_map"
+bpftool -m prog load bpf/sockops/bpf_redir.o "/sys/fs/bpf/bpf_redir" map name sock_ops_map pinned "/sys/fs/bpf/sock_ops_map"
 bpftool prog attach pinned "/sys/fs/bpf/bpf_redir" msg_verdict pinned "/sys/fs/bpf/sock_ops_map"
 
